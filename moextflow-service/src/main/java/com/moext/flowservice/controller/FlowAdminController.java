a8210e2c6e276471f2ca907976ae632aed063c0a
admin@moext.com <admin@moext.com>
2023-03-22 10:48:30 +0800

    重构

--- moextflow-service/src/main/java/com/moext/flowservice/controller/FlowAdminController.java
+++ moextflow-service/src/main/java/com/moext/flowservice/controller/FlowAdminController.java
@@ -1,26 +1,7 @@
 package com.moext.flowservice.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.moext.flowservice.common.BaseResponse;
-import com.moext.flowservice.common.PageResponse;
-import com.moext.flowservice.common.RspUtils;
-import com.moext.flowservice.data.gen.model.TodoTask;
-import com.moext.flowservice.dto.req.ApproveProcessReq;
-import com.moext.flowservice.dto.req.StartProcessReq;
-import com.moext.flowservice.dto.req.TodoIdReq;
-import com.moext.flowservice.dto.req.TodoListCountReq;
-import com.moext.flowservice.dto.req.TodoListPageReq;
-import com.moext.flowservice.service.TodoTaskService;
 
 /**
  * 流程管理接口
